ad8abb7c00653c8aa178d9897a0fc342ca58ee43
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-17 15:55:11 -0500

    refactor: copyToClipboard
    * refactor: copyToClipboard
    
    * fix: DrawText2D

--- client/vectors.lua
+++ client/vectors.lua
@@ -1,44 +1,28 @@
 function CopyToClipboard(dataType)
+    local coords = GetEntityCoords(cache.ped)
+    local x, y, z = math.round(coords.x, 2), math.round(coords.y, 2), math.round(coords.z, 2)
+    local heading = GetEntityHeading(cache.ped)
+    local h = math.round(heading, 2)
+
+    local data
+    local message
+
     if dataType == 'coords2' then
-        local coords = GetEntityCoords(cache.ped)
-        local x = math.round(coords.x, 2)
-        local y = math.round(coords.y, 2)
-
-        local string = string.format('vec2(%s, %s)', x, y)
-        lib.setClipboard(string)
-
-        exports.qbx_core:Notify(Lang:t('success.coords_copied'), 'success')
+        data = string.format('vec2(%s, %s)', x, y)
+        message = Lang:t('success.coords_copied')
     elseif dataType == 'coords3' then
-        local coords = GetEntityCoords(cache.ped)
-        local x = math.round(coords.x, 2)
-        local y = math.round(coords.y, 2)
-        local z = math.round(coords.z, 2)
-
-        local string = string.format('vec3(%s, %s, %s)', x, y, z)
-        lib.setClipboard(string)
-
-        exports.qbx_core:Notify(Lang:t('success.coords_copied'), 'success')
+        data = string.format('vec3(%s, %s, %s)', x, y, z)
+        message = Lang:t('success.coords_copied')
     elseif dataType == 'coords4' then
-        local coords = GetEntityCoords(cache.ped)
-        local x = math.round(coords.x, 2)
-        local y = math.round(coords.y, 2)
-        local z = math.round(coords.z, 2)
-        local heading = GetEntityHeading(cache.ped)
-        local h = math.round(heading, 2)
-
-        local string = string.format('vec4(%s, %s, %s, %s)', x, y, z, h)
-        lib.setClipboard(string)
-
-        exports.qbx_core:Notify(Lang:t('success.coords_copied'), 'success')
+        data = string.format('vec4(%s, %s, %s, %s)', x, y, z, h)
+        message = Lang:t('success.coords_copied')
     elseif dataType == 'heading' then
-        local heading = GetEntityHeading(cache.ped)
-        local h = math.round(heading, 2)
-
-        local string = h
-        lib.setClipboard(tostring(string))
-
-        exports.qbx_core:Notify(Lang:t('success.heading_copied'), 'success')
+        data = tostring(h)
+        message = Lang:t('success.heading_copied')
     end
+
+    lib.setClipboard(data)
+    exports.qbx_core:Notify(message, 'success')
 end
 
 RegisterNetEvent('qbx_admin:client:copyToClipboard', function(dataType)
