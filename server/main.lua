5e255b946aaa68438e0992cffa79cf99e22da24e
Swellington Soares <145943214+Swellington-Soares@users.noreply.github.com>
2024-09-29 06:49:55 -0300

    fix(server): use givekeys export correctly (#70)
    * refactor(server): use vehicle entity from spawnvehicle
    
    * chore: revert editorconfig linefeed change
    
    ---------
    
    Co-authored-by: Solareon <769465+solareon@users.noreply.github.com>

--- server/main.lua
+++ server/main.lua
@@ -57,7 +57,7 @@ RegisterNetEvent('qbx_admin:server:sendReply', function(report, message)
             local name = GetPlayerName(source)
 
             TriggerClientEvent('chatMessage', report.senderId, "", {255, 0, 0}, string.format('[REPORT #%s] [%s] ^7%s', report.id, name, message))
-            
+
             exports.qbx_core:Notify(source, locale('success.sent_report_reply'), 'success')
             if REPORTS[k].claimed == 'Nobody' then
                 REPORTS[k].claimed = name
@@ -289,15 +289,13 @@ end)
 
 lib.callback.register('qbx_admin:server:spawnVehicle', function(source, model)
     local ped = GetPlayerPed(source)
-    local netId = qbx.spawnVehicle({
+    local netId, vehicle = qbx.spawnVehicle({
         model = model,
         spawnSource = ped,
         warp = true,
     })
 
-    local plate = qbx.getVehiclePlate(NetworkGetEntityFromNetworkId(netId))
-
-    exports.qbx_vehiclekeys:GiveKeys(source, plate)
+    exports.qbx_vehiclekeys:GiveKeys(source, vehicle)
     return netId
 end)
 
